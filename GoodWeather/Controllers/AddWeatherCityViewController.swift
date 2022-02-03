cbdab00549dcb178b14fa3424e50ed44337b41a8
matthewogtong <ogtongm@gmail.com>
2022-02-03 11:53:30 -0500

    Adjust navigation bar title size

--- GoodWeather/Controllers/AddWeatherCityViewController.swift
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -8,7 +8,8 @@
 import Foundation
 import UIKit
 
-class AddWeatherCityTableViewController {
+class AddWeatherCityTableViewController: UIViewController {
+    
     
     
 }
--- GoodWeather/Controllers/WeatherListTableViewController.swift
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -8,7 +8,12 @@
 import Foundation
 import UIKit
 
-class WeatherListTableViewController {
+class WeatherListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
     
 }
 
