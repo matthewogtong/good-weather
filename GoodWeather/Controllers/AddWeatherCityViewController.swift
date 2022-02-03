28061d56530bdb632f24a964f90f03f96daf90c3
matthewogtong <ogtongm@gmail.com>
2022-02-03 11:43:04 -0500

    Create Controllers group, add WeatherListTableVC and AddWeatherCityVC and connect to VC storyboard VCs

---
+++ GoodWeather/Controllers/AddWeatherCityViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddWeatherCityViewController.swift
+//  GoodWeather
+//
+//  Created by Matthew Ogtong on 2/3/22.
+//
+
+import Foundation
+import UIKit
+
+class AddWeatherCityTableViewController {
+    
+    
+}
---
+++ GoodWeather/Controllers/WeatherListTableViewController.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherListTableViewController.swift
+//  GoodWeather
+//
+//  Created by Matthew Ogtong on 2/3/22.
+//
+
+import Foundation
+import UIKit
+
+class WeatherListTableViewController {
+    
+}
+
+
