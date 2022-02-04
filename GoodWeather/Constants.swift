b11c2f3ce775bee7899e52e459de8284f1cc913c
matthewogtong <ogtongm@gmail.com>
2022-02-04 10:25:42 -0500

    Create constants file and add global constant for resuesable weather cell

---
+++ GoodWeather/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  GoodWeather
+//
+//  Created by Matthew Ogtong on 2/4/22.
+//
+
+import Foundation
+
+struct K {
+    
+    static let reuseableWeatherCell = "WeatherCell"
+    
+}
