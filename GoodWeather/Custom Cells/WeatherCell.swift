139257aa138748ea519821a7c3c48a89568914f5
matthewogtong <ogtongm@gmail.com>
2022-02-05 17:20:28 -0500

    Create new group Custom Cells

---
+++ GoodWeather/Custom Cells/WeatherCell.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherCell.swift
+//  GoodWeather
+//
+//  Created by Matthew Ogtong on 2/5/22.
+//
+
+import Foundation
+import UIKit
+
+
+class WeatherCell: UITableViewCell {
+    
+    @IBOutlet weak var cityNameLabel: UILabel!
+    
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+}
