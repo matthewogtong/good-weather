90df0fa333adbc0b3f4aea4392a9547a6cf21049
matthewogtong <ogtongm@gmail.com>
2022-02-12 15:53:23 -0500

    Implement WebService Service

--- GoodWeather/Services/Webservice.swift
+++ GoodWeather/Services/Webservice.swift
@@ -8,13 +8,25 @@
 import Foundation
 
 struct Resource<T> {
+    
     let url: URL
     let parse: (Data) -> T?
+    
 }
 
-final class Webservice {
+final class WebService {
     
-    func load() {
+    func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
+        
+        URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            
+            if let data = data {
+                completion(resource.parse(data))
+            } else {
+                completion(nil)
+            }
+            
+        }.resume()
         
     }
     
