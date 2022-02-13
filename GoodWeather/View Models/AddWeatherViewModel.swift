450f6c694ae87e07af9309221749ec9fcb0f26bf
matthewogtong <ogtongm@gmail.com>
2022-02-13 11:32:52 -0500

    Implement addWeather method in AddWeatherViewModel

--- GoodWeather/View Models/AddWeatherViewModel.swift
+++ GoodWeather/View Models/AddWeatherViewModel.swift
@@ -9,8 +9,25 @@ import Foundation
 
 class AddWeatherViewModel {
     
-    func addWeather(for city: String) {
+    func addWeather(for city: String, @escaping (WeatherViewModel) -> Void) {
         
+        let weatherURL = Constants.Urls.urlForWeatherByCity(city: city)
+        
+        let weatherResource = Resource<WeatherResponse>(url: weatherURL) { data in
+            let weatherResponse = try? JSONDecoder().decode(WeatherResponse.self, from: data)
+            return weatherResponse
+        }
+        
+        WebService().load(resource: weatherResource) { result in
+            
+            if let weatherResponse = result {
+                
+                let vm = WeatherViewModel(weather: weatherResponse)
+                completion(vm)
+                
+            }
+            
+        }
         
     }
     
