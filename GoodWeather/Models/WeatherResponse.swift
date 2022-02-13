530b53b41ad1922fc95ebce2dcf2cd9fbbef3fca
matthewogtong <ogtongm@gmail.com>
2022-02-12 20:29:20 -0500

    Implementing models

---
+++ GoodWeather/Models/WeatherResponse.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherResponse.swift
+//  GoodWeather
+//
+//  Created by Matthew Ogtong on 2/12/22.
+//
+
+import Foundation
+
+struct WeatherResponse: Decodable {
+    
+    let main: Weather
+    
+}
+
+struct Weather: Decodable {
+    
+    let temp: Double
+    let humidity: Double
+    
+}
